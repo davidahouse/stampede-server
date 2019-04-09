5f0da55dbc32d4f52a4fb52a9cfe710faed98d01
David house <davidahouse@gmail.com>
2019-04-09 11:43:07 -0400

    :sparkles: start job now creates an identifier and pushes it to queue

--- api/startJob.js
+++ api/startJob.js
@@ -2,10 +2,25 @@ async function handle(req, res, redisClient) {
 
     const jobKey = 'job_' + req.params.job
     console.log(jobKey)
-    const jobDetails = await redisClient.get(jobKey)
+    const rawJobDetails = await redisClient.get(jobKey)
+    const jobDetails = JSON.parse(rawJobDetails)
+    let queue = 'jobDefaultQueue'
+    if (jobDetails.queue != null) {
+        queue = jobDetails.queue
+    }
 
-    await redisClient.lpush('jobRequests', jobDetails)
-    res.send(jobDetails)
+    const jobBuildKey = 'job_' + req.params.job + '_build'
+    await redisClient.incr(jobBuildKey)
+    let buildNumber = await redisClient.get(jobBuildKey)
+
+    const jobIdentifier = 'job_' + req.params.job + '_' + buildNumber
+
+    const status = {status: 'queued', dateQueued: new Date()}
+    const job = {queue: queue, status: status, build: buildNumber, details: jobDetails}
+    await redisClient.set(jobIdentifier, JSON.stringify(job))
+    await redisClient.expire(jobIdentifier, 3600)
+    await redisClient.lpush(queue, jobIdentifier)
+    res.send(job)
 }
 
 module.exports.handle = handle
