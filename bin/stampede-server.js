abf76c675f31e40a99d620a31bfd0864d657f541
David house <davidahouse@gmail.com>
2019-08-31 07:55:48 -0400

    :construction: Changes based on github app api

--- bin/stampede-server.js
+++ bin/stampede-server.js
@@ -2,19 +2,11 @@
 const chalk = require('chalk')
 const clear = require('clear')
 const figlet = require('figlet')
-const asyncRedis = require("async-redis")
-let express = require('express')
 const fs = require('fs')
-let app = express()
-const expressWs = require('express-ws')(app)
-let router = express.Router() // eslint-disable-line new-cap
-let bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use('/', router)
 
-// APIs
-const apiJobs = require('../api/jobs')
-const apiStartJob = require('../api/startJob')
+// Internal modules
+const web = require('../lib/web')
+const redis = require('../lib/redis')
 
 const conf = require('rc')('stampede', {
   // defaults
@@ -22,57 +14,22 @@ const conf = require('rc')('stampede', {
   redisPort: 6379,
   redisPassword: null,
   webPort: 7766,
-  jobFolder: '.'
+  githubAppID: 0,
+  githubAppPEMPath: null,
+  githubAppPEM: null,
+  githubHost: null
 })
-
-let client = createRedisClient()
-
-client.on('error', function(err) {
-  console.log('redis connect error: ' + err)
-})
-
-// TODO: Setup REST API routes
 
 clear()
-console.log(chalk.red(figlet.textSync('stampede server', {horizontalLayout: 'full'})))
+console.log(chalk.red(figlet.textSync('stampede', {horizontalLayout: 'full'})))
 console.log(chalk.red('Redis Host: ' + conf.redisHost))
 console.log(chalk.red('Redis Port: ' + conf.redisPort))
 console.log(chalk.red('Web Port: ' + conf.webPort))
+console.log(chalk.red('GitHub APP ID: ' + conf.githubAppID))
+console.log(chalk.red('GitHub PEM Path: ' + conf.githubAppPEMPath))
 
-app.ws('/socket', function(ws, req) {
-  ws.on('message', function(msg) {
-    logger.info('message from socket: msg')
-  })
-})
+const pem = fs.readFileSync(conf.githubAppPEMPath, 'utf8')
+conf.githubAppPEM = pem
 
-app.listen(conf.webPort, function() {
-  console.log(chalk.yellow('Listening on port: ' + conf.webPort))
-})
-
-router.get('/api/jobs', function(req, res) {
-  apiJobs.handle(req, res, client)
-})
-
-router.post('/api/startJob/:job', function(req, res) {
-  apiStartJob.handle(req, res, client)
-})
-
-let jobs = []
-const jobFiles = fs.readdirSync(conf.jobFolder).filter(function(file) {
-  const jobFile = fs.readFileSync(conf.jobFolder + '/' + file)
-  const job = JSON.parse(jobFile)
-  client.set('job_' + job.title, JSON.stringify(job))
-  jobs.push(job.title)
-})
-client.set('jobs', JSON.stringify(jobs))
-
-function createRedisClient() {
-  if (conf.redisPassword != null) {
-    return asyncRedis.createClient({host: conf.redisHost, 
-                               port: conf.redisPort, 
-                               password: conf.redisPassword})
-  } else {
-    return asyncRedis.createClient({host: conf.redisHost, 
-                               port: conf.redisPort})
-  }
-}
+redis.startRedis(conf)
+web.startRESTApi(conf, redis)
