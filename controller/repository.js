46178d1890385247a744a8e469abb03e6323b21b
David House <david.house@kp.org>
2019-09-10 07:39:59 -0400

    :construction: wip on repository screen

--- controller/repository.js
+++ controller/repository.js
@@ -6,11 +6,11 @@
  * @param {*} path 
  */
 async function handle(req, res, redisClient, path) {
-  const prTasks = await redisClient.fetch('stampede-' + req.query.org + '-' + req.query.repo + '-pullrequest', [])
-  console.dir(prTasks)
+  const pullRequestConfig = await redisClient.fetch('stampede-' + req.query.org + '-' + req.query.repo + '-pullrequest', {})
+  const builds = await redisClient.fetchKeys('stampede-' + req.query.org + '-' + req.query.repo + '-pullrequest-*')
+  console.dir(builds)
   res.render(path + 'repository', {org: req.query.org, repo: req.query.repo, 
-                                    prConfig: Object.entries(prTasks.config),
-                                    prTasks: prTasks.tasks})
+                                    pullRequestConfig: JSON.stringify(pullRequestConfig, null, 2)})
 }
 
 module.exports.handle = handle
