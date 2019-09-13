564efdf7a178e5865594c5cb5d24e93d9874739c
Tyson Black <tyson.black@me.com>
2019-09-12 22:39:34 -0700

    Adding Use Strict

--- bin/stampede-server.js
+++ bin/stampede-server.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+'use strict'
 const chalk = require('chalk')
 const clear = require('clear')
 const figlet = require('figlet')
--- controller/tasks.js
+++ controller/tasks.js
@@ -1,3 +1,4 @@
+'use strict'
 /**
  * handle tasks
  * @param {*} req 
