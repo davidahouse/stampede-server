8d74cd09d97c6b7014b7f190a655a4a298449401
David House <david.house@kp.org>
2019-09-23 20:11:05 -0400

    :green_check_mark: Test setup

---
+++ test/stampede-server-tests.js
@@ -0,0 +1,12 @@
+'use strict'
+/* eslint-env mocha */
+const expect = require('chai').expect
+
+describe('Stampede Server', function() {
+  beforeEach(() => {
+  })
+
+  it('should be tested', function() {
+    expect(true).to.equal(true)
+  })
+})
