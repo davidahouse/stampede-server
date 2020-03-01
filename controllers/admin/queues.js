9b648d3cdf8149a825b602ad297e92317b574768
David House <david.house@kp.org>
2020-02-29 20:54:21 -0500

    :recycle: Admin route refactor

--- controllers/admin/queues.js
+++ controllers/admin/queues.js
@@ -1,16 +1,22 @@
 /**
+ * path this handler will serve
+ */
+function path() {
+  return "/admin/queues";
+}
+
+/**
  * handle tasks
  * @param {*} req
  * @param {*} res
- * @param {*} cache
- * @param {*} db
- * @param {*} path
+ * @param {*} dependencies
  */
-async function handle(req, res, cache, db, path) {
-  const queueList = await cache.systemQueues.fetchSystemQueues();
-  res.render(path + "admin/queues", {
+async function handle(req, res, dependencies) {
+  const queueList = await dependencies.cache.systemQueues.fetchSystemQueues();
+  res.render(dependencies.viewsPath + "admin/queues", {
     queues: queueList != null ? queueList : []
   });
 }
 
+module.exports.path = path;
 module.exports.handle = handle;
