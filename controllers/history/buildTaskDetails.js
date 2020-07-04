70d9d7d8f3e0978b9817560bb0cdd68118cc3e3d
David House <davidahouse@gmail.com>
2020-07-04 08:17:53 -0400

    :recycle: Changed retention to use archiving instead of deleting

--- controllers/history/buildTaskDetails.js
+++ controllers/history/buildTaskDetails.js
@@ -18,76 +18,80 @@ async function handle(req, res, dependencies, owners) {
     const detailsRows = await dependencies.db.fetchTaskDetails(
       req.query.taskID
     );
-    const taskDetails = detailsRows.rows[0];
     const configValues = [];
-    if (req.validAdminSession == true) {
-      Object.keys(
-        taskDetails.details.config != null ? taskDetails.details.config : {}
-      ).forEach(function (key) {
-        configValues.push({
-          key: key,
-          value: taskDetails.details.config[key].value,
-          source: taskDetails.details.config[key].source,
+    const scmDetails = [];
+    let artifacts = [];
+    let taskDetails = { details: {} };
+    if (detailsRows.rows.length > 0) {
+      taskDetails = detailsRows.rows[0];
+      if (req.validAdminSession == true) {
+        Object.keys(
+          taskDetails.details.config != null ? taskDetails.details.config : {}
+        ).forEach(function (key) {
+          configValues.push({
+            key: key,
+            value: taskDetails.details.config[key].value,
+            source: taskDetails.details.config[key].source,
+          });
         });
-      });
+      }
+      artifacts =
+        taskDetails.details.result != null &&
+        taskDetails.details.result.artifacts != null
+          ? taskDetails.details.result.artifacts
+          : [];
+
+      if (taskDetails.details.scm.pullRequest != null) {
+        scmDetails.push({
+          title: "PR Number",
+          value: taskDetails.details.scm.pullRequest.number,
+        });
+        scmDetails.push({
+          title: "Head",
+          value: taskDetails.details.scm.pullRequest.head.ref,
+        });
+        scmDetails.push({
+          title: "Head SHA",
+          value: taskDetails.details.scm.pullRequest.head.sha,
+        });
+        scmDetails.push({
+          title: "Base",
+          value: taskDetails.details.scm.pullRequest.base.ref,
+        });
+        scmDetails.push({
+          title: "Base SHA",
+          value: taskDetails.details.scm.pullRequest.base.sha,
+        });
+      } else if (taskDetails.details.scm.branch != null) {
+        scmDetails.push({
+          title: "Branch",
+          value: taskDetails.details.scm.branch.name,
+        }),
+          scmDetails.push({
+            title: "SHA",
+            value: taskDetails.details.scm.branch.sha,
+          });
+        scmDetails.push({
+          title: "Commit",
+          value: taskDetails.details.scm.commitMessage,
+        });
+      } else if (taskDetails.details.scm.release != null) {
+        scmDetails.push({
+          title: "Release",
+          value: taskDetails.details.scm.release.name,
+        });
+        scmDetails.push({
+          title: "Tag",
+          value: taskDetails.details.scm.release.tag,
+        });
+        scmDetails.push({
+          title: "SHA",
+          value: taskDetails.details.scm.release.sha,
+        });
+      }
     }
     const buildRows = await dependencies.db.fetchBuild(task.build_id);
     const build = buildRows.rows[0];
-    const artifacts =
-      taskDetails.details.result != null &&
-      taskDetails.details.result.artifacts != null
-        ? taskDetails.details.result.artifacts
-        : [];
-
-    const scmDetails = [];
-    if (taskDetails.details.scm.pullRequest != null) {
-      scmDetails.push({
-        title: "PR Number",
-        value: taskDetails.details.scm.pullRequest.number,
-      });
-      scmDetails.push({
-        title: "Head",
-        value: taskDetails.details.scm.pullRequest.head.ref,
-      });
-      scmDetails.push({
-        title: "Head SHA",
-        value: taskDetails.details.scm.pullRequest.head.sha,
-      });
-      scmDetails.push({
-        title: "Base",
-        value: taskDetails.details.scm.pullRequest.base.ref,
-      });
-      scmDetails.push({
-        title: "Base SHA",
-        value: taskDetails.details.scm.pullRequest.base.sha,
-      });
-    } else if (taskDetails.details.scm.branch != null) {
-      scmDetails.push({
-        title: "Branch",
-        value: taskDetails.details.scm.branch.name,
-      }),
-        scmDetails.push({
-          title: "SHA",
-          value: taskDetails.details.scm.branch.sha,
-        });
-      scmDetails.push({
-        title: "Commit",
-        value: taskDetails.details.scm.commitMessage,
-      });
-    } else if (taskDetails.details.scm.release != null) {
-      scmDetails.push({
-        title: "Release",
-        value: taskDetails.details.scm.release.name,
-      });
-      scmDetails.push({
-        title: "Tag",
-        value: taskDetails.details.scm.release.tag,
-      });
-      scmDetails.push({
-        title: "SHA",
-        value: taskDetails.details.scm.release.sha,
-      });
-    }
 
     res.render(dependencies.viewsPath + "history/buildTaskDetails", {
       owners: owners,
