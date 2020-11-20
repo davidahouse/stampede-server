530862c176ab90ad7fc1bcd3ef0588e1b106e618
David House <davidahouse@gmail.com>
2020-11-19 20:25:17 -0500

    :recycle: Separate task and system queues in display and api

--- api/monitor/queueSummary.js
+++ api/monitor/queueSummary.js
@@ -26,14 +26,54 @@ async function handle(req, res, dependencies) {
   const queues = [];
 
   for (let index = 0; index < queueList.length; index++) {
-    const q = new Queue("stampede-" + queueList[index].id, redisConfig);
-    const stats = await q.getJobCounts();
+    let stats = await queueStats("stampede-" + queueList[index].id, redisConfig)
     queues.push({
       queue: queueList[index].id,
       stats: stats,
     });
   }
-  res.send(queues);
+
+  const systemQueues = []
+  let incomingStats = await queueStats(dependencies.serverConfig.incomingQueue, redisConfig)
+  systemQueues.push({
+    queue: dependencies.serverConfig.incomingQueue,
+    stats: incomingStats
+  })
+  let responseStats = await queueStats(dependencies.serverConfig.responseQueue, redisConfig)
+  systemQueues.push({
+    queue: dependencies.serverConfig.responseQueue,
+    stats: responseStats
+  })
+  if (dependencies.serverConfig.handleSlackNotifications === "enabled") {
+    let slackStats = await queueStats("slack-notifications", dependencies.redisConfig)
+    systemQueues.push({
+      queue: "slack-notifications",
+      stats: slackStats
+    })
+  }
+  if (dependencies.serverConfig.handlePRCommentNotifications === "enabled") {
+    let prCommentStats = await queueStats("prcomment-notifications", dependencies.redisConfig)
+    systemQueues.push({
+      queue: "prcomment-notifications",
+      stats: prCommentStats
+    })
+  }
+
+  res.send({ taskQueues: queues, systemQueues: systemQueues });
+}
+
+/**
+ * Collect queue stats
+ * @param {*} queue 
+ * @param {*} redisConfig 
+ */
+async function queueStats(queue, redisConfig) {
+  const q = new Queue(
+    "stampede-" + queue,
+    redisConfig
+  );
+  const stats = await q.getJobCounts();
+  return stats
 }
 
 /**
