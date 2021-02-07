1dcf28c22587494cb6d2d0c42bfb81fd3da5857b
David House <davidahouse@gmail.com>
2021-02-07 12:01:10 -0500

    :fire: Removed custom artifact rendering

--- controllers/repositories/taskDetails.js
+++ controllers/repositories/taskDetails.js
@@ -108,44 +108,11 @@ async function handle(req, res, dependencies, owners) {
   if (artifactRows != null && artifactRows.rows != null) {
     for (let aindex = 0; aindex < artifactRows.rows.length; aindex++) {
       const artifact = artifactRows.rows[aindex];
-      if (artifact.type == "cloc") {
-        artifact.url =
-          "/artifacts/viewCloc?taskID=" +
-          task.task_id +
-          "&artifact=" +
-          encodeURI(artifact.title);
-      } else if (artifact.type == "xcodebuild") {
-        artifact.url =
-          "/artifacts/viewXcodebuild?taskID=" +
-          task.task_id +
-          "&artifact=" +
-          encodeURI(artifact.title);
-      } else if (artifact.type == "imagegallery") {
-        artifact.url =
-          "/artifacts/viewImageGallery?taskID=" +
-          task.task_id +
-          "&artifact=" +
-          encodeURI(artifact.title);
-      } else if (artifact.type == "imagediff") {
-        artifact.url =
-          "/artifacts/viewImageGalleryDiff?taskID=" +
-          task.task_id +
-          "&artifact=" +
-          encodeURI(artifact.title);
-      } else if (artifact.type == "download") {
-      } else if (artifact.type == "link") {
-      } else if (artifact.type == "installplist") {
+      if (artifact.type == "installplist") {
         artifact.url =
           "itms-services://?action=download-manifest&url=" +
           encodeURIComponent(artifact.url);
-      } else {
-        artifact.url =
-          "/artifacts/viewUnknown?taskID=" +
-          task.task_id +
-          "&artifact=" +
-          encodeURI(artifact.title);
       }
-
       artifacts.push(artifact);
     }
   }
